d1c7b49c8831798d2aab30a1f7c9b2b5d3fce2f4
imran <87297004+imranshaikh9930@users.noreply.github.com>
2024-04-27 12:01:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 //your code here
+
+function setClock(){
+	
+
+const date = new Date();
+const hr = date.getHours() % 12;
+const mi = date.getMinutes();
+const se = date.getSeconds();
+
+const hour = document.querySelector(".hour-hand");
+const min = document.querySelector(".min-hand");
+const sec = document.querySelector(".second-hand");
+
+const hourR = (hr * 30) + (0.5 * mi);
+const minR = (mi * 6) + (0.1 * se);
+const secR = se * 6;
+
+hour.style.transform = `rotate(${hourR}deg)`;
+hour.style.transform = `rotate(${minR}deg)`;
+hour.style.transform = `rotate(${secR}deg)`;
+}
+
+setInterval(setClock,1000)
